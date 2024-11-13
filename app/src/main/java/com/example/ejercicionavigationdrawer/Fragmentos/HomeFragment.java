91b586eadab23ad826f5785fd65a8f9c9b712ab5
jrodcab519 <jrodcab519@g.educaand.es>
2024-11-13 11:57:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ejercicionavigationdrawer/Fragmentos/HomeFragment.java
@@ -0,0 +1,38 @@
+package com.example.ejercicionavigationdrawer.Fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ejercicionavigationdrawer.R;
+import com.example.ejercicionavigationdrawer.databinding.FragmentHomeBinding;
+
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
